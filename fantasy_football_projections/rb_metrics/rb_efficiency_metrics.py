d22207b24f0c17644ba68770d8e72e85cc27f187
JakeThompson16 <jake.thompson001@umb.edu>
2026-01-15 18:48:56 -0500

    New RB efficiency metrics

---
+++ fantasy_football_projections/rb_metrics/rb_efficiency_metrics.py
@@ -0,0 +1,235 @@
+
+import polars as pl
+from fantasy_football_projections.config import PPR, POINTS_PER_RUSH_YARD, POINTS_PER_RUSH_TD, \
+    POINTS_PER_REC_YARD, POINTS_PER_REC_TD, EXPLOSIVE_RUN, EXPLOSIVE_RECEPTION
+from fantasy_football_projections.data_loading.player_data import load_player_stats, load_pbp_data, get_rb_ids
+
+# Metrics measuring a players ability to capitalize on their opportunities
+def rb_efficiency_metrics(seasons: list[int] | int)->pl.DataFrame:
+    """
+    Returns a data frame of feature cols for rb efficiency metrics, cols found
+    in get_rb_efficiency_cols()
+    :param seasons: List of seasons to get data for
+    :return: Data frame of weekly efficiency metrics for every rb in seasons
+    """
+
+    df = load_player_stats(seasons).filter(pl.col("position") == "RB")
+    pbp_stats = load_pbp_data(seasons)
+
+    rb_ids = get_rb_ids()
+
+    # Filters pbp carries
+    pbp_carries = pbp_stats.filter(pl.col("rusher_player_id").is_in(rb_ids))
+
+    # Filters pbp targets
+    pbp_targets = pbp_stats.filter(pl.col("receiver_player_id").is_in(rb_ids))
+
+    # Generates needed stat cols in pbp_carries
+    pbp_carries = pbp_carries.with_columns(
+
+        # Redzone Carries
+        pl.when(pl.col("yardline_100") <= 20)
+        .then(1)
+        .otherwise(0)
+        .alias("redzone_carry"),
+
+        # Redzone touchdowns
+        pl.when((pl.col("yardline_100") <= 20) & (pl.col("rush_touchdown") == 1))
+        .then(1)
+        .otherwise(0)
+        .alias("redzone_td_rush"),
+
+        # Positive epa play
+        pl.col("success")
+        .alias("successful_rush"),
+
+        # Rush of 10+ yards
+        pl.when(pl.col("yards_gained") >= EXPLOSIVE_RUN)
+        .then(1)
+        .otherwise(0)
+        .alias("explosive_rush"),
+
+        # Fantasy points gained
+        (pl.col("yards_gained") * POINTS_PER_RUSH_YARD +
+        pl.col("rush_touchdown") * POINTS_PER_RUSH_TD)
+        .alias("rush_fpoints_gained")
+    )
+
+    # Generates needed stat cols in pbp_targets
+    pbp_targets = pbp_targets.with_columns(
+
+        # Redzone targets
+        pl.when(pl.col("yardline_100") <= 20)
+        .then(1)
+        .otherwise(0)
+        .alias("redzone_target"),
+
+        # Redzone touchdowns
+        pl.when((pl.col("yardline_100") <= 20) & (pl.col("pass_touchdown") == 1))
+        .then(1)
+        .otherwise(0)
+        .alias("redzone_td_reception"),
+
+        # Positive epa play
+        pl.col("success")
+        .alias("successful_target"),
+
+        # Reception of 12+ yards
+        pl.when(pl.col("yards_gained") >= EXPLOSIVE_RECEPTION)
+        .then(1)
+        .otherwise(0)
+        .alias("explosive_target"),
+
+        # Fantasy points gained
+        pl.when(pl.col("complete_pass") == 0)
+        .then(0)
+        .otherwise(
+            1 * PPR +
+            pl.col("yards_gained") * POINTS_PER_REC_YARD +
+            pl.col("pass_touchdown") * POINTS_PER_REC_TD
+        )
+        .alias("rec_fpoints_gained")
+    )
+
+    # Aggregates pbp carries to weekly totals
+    pbp_carries = pbp_carries.group_by(["week", "season", "rusher_player_id"]).agg(
+
+        # Redzone opportunities
+        pl.col("redzone_carry").sum().alias("redzone_carries"),
+
+        # Redzone touchdowns
+        pl.col("redzone_td_rush").sum().alias("redzone_td_rushes"),
+
+        # Successful plays
+        pl.col("successful_rush").sum().alias("successful_rushes"),
+
+        # Fantasy points gained
+        pl.col("rush_fpoints_gained").sum().alias("rush_fpoints_gained"),
+
+        # Explosive plays
+        pl.col("explosive_rush").sum().alias("explosive_rushes")
+    )
+
+    # Aggregates pbp receptions to weekly totals
+    pbp_targets = pbp_targets.group_by(["week", "season", "receiver_player_id"]).agg(
+
+        # Redzone opportunities
+        pl.col("redzone_target").sum().alias("redzone_targets"),
+
+        # Redzone touchdowns
+        pl.col("redzone_td_reception").sum().alias("redzone_td_receptions"),
+
+        # Successful plays
+        pl.col("successful_target").sum().alias("successful_targets"),
+
+        # Fantasy points gained
+        pl.col("rec_fpoints_gained").sum().alias("rec_fpoints_gained"),
+
+        # Explosive plays
+        pl.col("explosive_target").sum().alias("explosive_receptions")
+    )
+
+    # Renames week and season in pbp_carries and pbp_targets to avoid duplicate error on the second join
+    pbp_carries = pbp_carries.rename({
+        "week": "c_week",      # polars renames these cols to week/season_right and tries to do
+        "season": "c_season"   # so with df.join 16 lines down but throws duplicate col error
+    })
+    pbp_targets = pbp_targets.rename({
+        "week": "t_week",
+        "season": "t_season"
+    })
+
+    # Joins pbp carries and targets
+    pbp_stats = pbp_carries.join(
+        pbp_targets,
+        left_on=["c_week", "c_season", "rusher_player_id"],
+        right_on=["t_week", "t_season", "receiver_player_id"],
+        how="outer"
+    )
+
+    # Joins pbp stats and player stats
+    df = df.join(
+        pbp_stats,
+        left_on=["week", "season", "player_id"],
+        right_on=["c_week", "c_season", "rusher_player_id"],
+        how="outer"
+    ).fill_null(0)
+
+    # Generates efficiency features
+    df = df.with_columns(
+
+        # Redzone rushing efficiency
+        pl.when(pl.col("redzone_carries") > 0)
+        .then(pl.col("redzone_td_rushes") / pl.col("redzone_carries"))
+        .otherwise(0)
+        .alias("redzone_carry_efficiency"),
+
+        # Redzone target efficiency
+        pl.when(pl.col("redzone_targets") > 0)
+        .then(pl.col("redzone_td_receptions") / pl.col("redzone_targets"))
+        .otherwise(0)
+        .alias("redzone_target_efficiency"),
+
+        # Yards per carry
+        pl.when(pl.col("carries") > 0)
+        .then(pl.col("rushing_yards") / pl.col("carries"))
+        .otherwise(0)
+        .alias("yards_per_carry"),
+
+        # Yards per target
+        pl.when(pl.col("targets") > 0)
+        .then(pl.col("receiving_yards") / pl.col("targets"))
+        .otherwise(0)
+        .alias("yards_per_target"),
+
+        # Successful carry rate
+        pl.when(pl.col("carries") > 0)
+        .then(pl.col("successful_rushes") / pl.col("carries"))
+        .otherwise(0)
+        .alias("successful_carry_rate"),
+
+        # Successful target rate
+        pl.when(pl.col("targets") > 0)
+        .then(pl.col("successful_targets") / pl.col("targets"))
+        .otherwise(0)
+        .alias("successful_target_rate"),
+
+        # Explosive run rate
+        pl.when(pl.col("carries") > 0)
+        .then(pl.col("explosive_rushes") / pl.col("carries"))
+        .otherwise(0)
+        .alias("explosive_carry_rate"),
+
+        # Explosive reception rate
+        pl.when(pl.col("targets") > 0)
+        .then(pl.col("explosive_receptions") / pl.col("targets"))
+        .otherwise(0)
+        .alias("explosive_reception_rate"),
+
+        # Fantasy points per carry
+        pl.when(pl.col("carries") > 0)
+        .then(pl.col("rush_fpoints_gained") / pl.col("carries"))
+        .otherwise(0)
+        .alias("fpoints_per_carry"),
+
+        # Fantasy points per target
+        pl.when(pl.col("targets") > 0)
+        .then(pl.col("rec_fpoints_gained") / pl.col("targets"))
+        .otherwise(0)
+        .alias("fpoints_per_target"),
+    )
+
+    cols = get_rb_efficiency_cols() + ["player_id", "week", "season"]
+    return df.select(cols)
+
+def get_rb_efficiency_cols()->list[str]:
+    """
+    Cols from rb_efficiency_metrics() also include 'week', 'season', 'player_id'
+    :return: list of column names of df returned by rb_efficiency_metrics()
+    """
+    return [
+        "redzone_carry_efficiency", "redzone_target_efficiency",
+        "yards_per_carry", "yards_per_target", "successful_carry_rate",
+        "successful_target_rate", "explosive_carry_rate", "explosive_reception_rate",
+        "fpoints_per_carry", "fpoints_per_target"
+    ]
---
+++ fantasy_football_projections/rb_metrics/rb_opportunity_metrics.py
@@ -0,0 +1,135 @@
+
+from functools import lru_cache
+import polars as pl
+from polars import Int32
+from fantasy_football_projections.config import CURRENT_SEASON
+from fantasy_football_projections.data_loading.player_data import load_snap_shares, load_player_stats, \
+    load_ff_opportunity_data, get_rb_ids
+from fantasy_football_projections.rb_metrics.utility import rusher_opportunity_bounds
+
+@lru_cache(maxsize=None)
+def get_rb_snap_shares(*seasons):
+    """
+    :param seasons: Seasons to get rb snap share data for
+    :return polars.df: Snap shares filtered to just RB's
+    """
+    snap_shares = load_snap_shares(*seasons)
+    rb_snap_shares = snap_shares.filter(pl.col("position") == "RB")
+
+    return rb_snap_shares
+
+def rb_opportunity_scores(seasons: list[int] | int)->pl.DataFrame:
+    """
+    Scores signifying a players rushing/receiving opportunity rate using snap share and carries
+    - keys: rushing_opportunity, receiving_opportunity
+    :param seasons: Seasons to get opportunity rates for
+    :return pl.DataFrame: Number in range [0, 1] where closer to 1 implies more opportunities
+    for rushing and receiving (week to week scores can technically exceed 1, but averages over
+    many games will not)
+    """
+    if type(seasons) == int:
+        seasons = [seasons]
+    rb_ids = get_rb_ids()
+
+    # Loads relevant statistics, filtering each df by player id
+    opportunity_data = get_rb_snap_shares(*seasons).filter(pl.col("gsis_id").is_in(rb_ids))
+    player_stats = load_player_stats(*seasons).filter(pl.col("player_id").is_in(rb_ids))
+    ff_opp = load_ff_opportunity_data(*seasons).select(
+        [
+            "rush_yards_gained_exp", "rush_touchdown_exp", "rush_first_down_exp",
+            "rec_yards_gained_exp", "rec_touchdown_exp", "rec_first_down_exp",
+            "receptions_exp", "player_id", "week", "season"
+        ]
+    ).with_columns(
+        pl.col("week").cast(Int32),
+        pl.col("season").cast(Int32)
+    )
+
+    # Renames week, season cols to avoid polars duplicate error
+    player_stats = player_stats.rename({
+        "week": "p_week",
+        "season": "p_season"
+    })
+    ff_opp = ff_opp.rename({
+        "week": "f_week",
+        "season": "f_season"
+    })
+
+    # Joins all statistics to one data-frame
+    opportunity_data = opportunity_data.join(
+        player_stats,
+        left_on=["week", "season", "gsis_id"],
+        right_on=["p_week", "p_season", "player_id"],
+        how="outer"
+    )
+    opportunity_data = opportunity_data.join(
+        ff_opp,
+        left_on=["week", "season", "player_id"],
+        right_on=["f_week", "f_season", "player_id"],
+        how="outer"
+    )
+
+    opportunity_data = opportunity_data.filter(pl.col("player_id").is_not_null())
+    opportunity_data = opportunity_data.fill_null(0)
+    opportunity_data = opportunity_data.sort(["week", "season"])
+
+    # Gets statistical bounds for statistics in current season
+    bounds = rusher_opportunity_bounds(CURRENT_SEASON)
+    car_hi = bounds["carries_upper"]
+    rye_hi = bounds["rush_yards_exp_upper"]
+    rtd_hi = bounds["rush_td_exp_upper"]
+    rfd_hi = bounds["rush_first_down_exp_upper"]
+    tar_hi = bounds["targets_upper"]
+    rec_hi = bounds["receptions_exp_upper"]
+    reye_hi = bounds["rec_yards_exp_upper"]
+    retd_hi = bounds["rec_td_exp_upper"]
+    refd_hi = bounds["rec_first_down_exp_upper"]
+
+    # Generates weighted statistics bounding data to [0,1]
+    opportunity_data = opportunity_data.with_columns(
+
+        # Rushing stats
+        (pl.col("rush_yards_gained_exp") / rye_hi).alias("weighted_rush_yards_exp"),
+        (pl.col("rush_touchdown_exp") / rtd_hi).alias("weighted_rush_touchdown_exp"),
+        (pl.col("rush_first_down_exp") / rfd_hi).alias("weighted_rush_first_down_exp"),
+        (pl.col("carries") / car_hi).alias("weighted_carries"),
+
+        # Receiving stats
+        (pl.col("targets") / tar_hi).alias("weighted_targets"),
+        (pl.col("rec_yards_gained_exp") / reye_hi).alias("weighted_rec_yards_exp"),
+        (pl.col("rec_touchdown_exp") / retd_hi).alias("weighted_rec_touchdown_exp"),
+        (pl.col("rec_first_down_exp") / refd_hi).alias("weighted_rec_first_down_exp"),
+        (pl.col("receptions_exp") / rec_hi).alias("receptions_exp_upper")
+    )
+
+    # Generates rushing opportunity score column for each game
+    opportunity_data = opportunity_data.with_columns(
+
+        # Rushing opportunity
+        (pl.col("offense_pct") * .30 +
+        pl.col("weighted_carries") * .25 +
+        pl.col("weighted_rush_yards_exp") * .20 +
+        pl.col("weighted_rush_touchdown_exp") * .15 +
+        pl.col("weighted_rush_first_down_exp") * .10)
+        .alias("rushing_opportunity"),
+
+        # Receiving opportunity
+        (pl.col("weighted_targets") * .30 +
+         pl.col("receptions_exp_upper") * .25 +
+        pl.col("weighted_rec_yards_exp") * .20 +
+        pl.col("weighted_rec_touchdown_exp") * .15 +
+        pl.col("weighted_rec_first_down_exp") * .10)
+        .alias("receiving_opportunity")
+    )
+
+    cols = get_rb_opportunity_cols() + ["player_id", "week", "season"]
+    return opportunity_data.select(cols)
+
+def get_rb_opportunity_cols()->list[str]:
+    """
+    Cols also include 'week', 'season', and 'player_id'
+    :return: List of cols in df returned by rb_opportunity_scores
+    """
+    return [
+        "rushing_opportunity", "receiving_opportunity"
+    ]
