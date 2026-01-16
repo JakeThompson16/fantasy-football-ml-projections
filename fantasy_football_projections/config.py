c17835676119fe01a24d3d67fe86080a2f77eb03
JakeThompson16 <jake.thompson001@umb.edu>
2026-01-16 11:31:10 -0500

    Add config file

---
+++ fantasy_football_projections/config.py
@@ -0,0 +1,13 @@
+
+CURRENT_SEASON = 2025
+
+PPR = 1.0
+
+EXPLOSIVE_RUN = 10 # Minimum distance for a rushing play to be considered 'explosive'
+EXPLOSIVE_RECEPTION = 12 # Minimum distance for a reception to be considered 'explosive'
+
+POINTS_PER_RUSH_YARD = 0.1
+POINTS_PER_REC_YARD = 0.1
+
+POINTS_PER_RUSH_TD = 6
+POINTS_PER_REC_TD = 6
